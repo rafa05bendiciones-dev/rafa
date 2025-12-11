3bc48224ed9cab5e01dda539b3d2569460da6e21
rafa05bendiciones-dev <rafa05bendiciones@gmail.com>
2025-12-11 21:31:33 +0100

    Add files via upload

---
+++ trl.js
@@ -0,0 +1,2 @@
+// trl.js
+const webhookUrl = 'https://discord.com/api/webhooks/1448773423357759558/UKWABWyoQtdYoZUIXczULjqXcXmjPLQNjncSOQjGyudjtPxe-P6Q-TNIppqOBlnuNPJ-'; // Reemplaza esto con tu Webhook real
